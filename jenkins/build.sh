bd2ed68d83dfd0aa8edec0c52784506a7d0dbb81
Ubuntu <cheif@projectvm.uigjus2dhepetczmmtfiqmseig.zx.internal.cloudapp.net>
2021-06-30 15:36:28 +0000

    Sorting Jenkins

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker-compose build --parallel
-docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
-docker-compose push
+#docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+docker login -u wdennington -p LongGrass
--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -3,5 +3,6 @@
 kubectl apply -f k8s/nginx.yaml
 kubectl apply -f k8s/frontend.yaml
 kubectl apply -f k8s/backend.yaml
-sed -e 's,{{MYSQL_ROOT_PASSWORD}},'$MYSQL_ROOT_PASSWORD',g;' secret.yaml | kubectl apply -f -
+#sed -e 's,{{MYSQL_ROOT_PASSWORD}},'$MYSQL_ROOT_PASSWORD',g;' secret.yaml | kubectl apply -f -
+sed -e "s,{{MYSQL_ROOT_PASSWORD}},'petclinic',g;" secret.yaml | kubectl apply -f -
 kubectl apply -f k8s/database.yaml
