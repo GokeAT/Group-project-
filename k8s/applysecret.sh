162049f485df4938acd447e1692a159bf449862d
Ubuntu <henil@jenkins.zuyfmmfu2ybevfxt0vemjrrlzf.zx.internal.cloudapp.net>
2021-06-30 10:07:22 +0000

    file name changed

--- k8s/applysecret.sh
+++ k8s/applysecret.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -e 's,{{MYSQL_ROOT_PASSWORD}},'$MYSQL_ROOT_PASSWORD',g;' secrets.yaml | kubectl apply -f -
+sed -e 's,{{MYSQL_ROOT_PASSWORD}},'$MYSQL_ROOT_PASSWORD',g;' secret.yaml | kubectl apply -f -
