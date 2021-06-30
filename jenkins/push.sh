1d9353fceefc5d099490ca069a00d4fc7ab274f7
Ubuntu <cheif@projectvm.uigjus2dhepetczmmtfiqmseig.zx.internal.cloudapp.net>
2021-06-30 16:04:50 +0000

    Sorting Jenkins

--- jenkins/push.sh
+++ jenkins/push.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-docker tag frontend wdennington/frontend:latest
-docker tag backend wdennington/backend:latest
-docker tag proxy wdennington/proxy:latest
-docker tag database wdennington/database:latest
+sudo docker tag frontend wdennington/frontend:latest
+sudo docker tag backend wdennington/backend:latest
+sudo docker tag proxy wdennington/proxy:latest
+sudo docker tag database wdennington/database:latest
 
-docker push wdennington/frontend:latest
-docker push wdennington/backend:latest
-docker push wdennington/proxy:latest
-docker push wdennington/database:latest
+sudo docker push wdennington/frontend:latest
+sudo docker push wdennington/backend:latest
+sudo docker push wdennington/proxy:latest
+sudo docker push wdennington/database:latest
 
