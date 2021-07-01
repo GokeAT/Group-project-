7e1f60c6f707d8bd06cc3904dfc19b5a89c44353
Ubuntu <absgeorge@jenkins-vm.rb02cr2yuzdellupgifue222oe.ax.internal.cloudapp.net>
2021-07-01 15:02:00 +0000

    terraform cluster changes

--- jenkins/dependencies.sh
+++ jenkins/dependencies.sh
@@ -37,6 +37,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install v14.16.0
+nvm install v14.16.0 -y
 sudo apt-get install npm
 npm install -g @angular/cli
