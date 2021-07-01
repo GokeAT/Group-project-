a0af76598caae986e8314b59512279332a801771
Ubuntu <cheif@projectvm.uigjus2dhepetczmmtfiqmseig.zx.internal.cloudapp.net>
2021-07-01 12:13:28 +0000

    Sorting Jenkins

--- jenkins/dependencies.sh
+++ jenkins/dependencies.sh
@@ -26,8 +26,8 @@ rm terraform_*_linux_*.zip
 #Installs Docker
 curl https://get.docker.com | sudo bash
 sudo groupadd docker
-sudo usermod -aG docker ${USER}
-sudo su - ${USER}
+sudo usermod -aG docker ${whoami}
+sudo su - ${whoami}
 sudo newgrp docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
