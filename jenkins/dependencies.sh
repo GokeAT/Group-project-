f3b902e781d729eee00c3c38b6d19cfb991eef2a
Ubuntu <absgeorge@jenkins-vm.rb02cr2yuzdellupgifue222oe.ax.internal.cloudapp.net>
2021-07-01 14:28:58 +0000

    removing service principal from terraform.sh

--- jenkins/dependencies.sh
+++ jenkins/dependencies.sh
@@ -17,11 +17,10 @@ mv ./kubectl ~/.local/bin/kubectl
 kubectl version --client
 
 #Installs terraform
-sudo apt install -y unzip wget
-wget https://releases.hashicorp.com/terraform/0.14.6/terraform_0.14.6_linux_amd64.zip
-unzip terraform_*_linux_*.zip
-sudo mv terraform /usr/local/bin/
-rm terraform_*_linux_*.zip
+
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt install terraform
 
 #Installs Docker
 curl https://get.docker.com | sudo bash
--- jenkins/terraform.sh
+++ jenkins/terraform.sh
@@ -3,56 +3,63 @@
 sudo apt install jq -y
 
 # #login and follow prompts
-az login
+az login --identity
 
-export TENANT_ID="$(az account show | jq -r '.tenantId')"
+# export TENANT_ID="$(az account show | jq -r '.tenantId')"
 
-# view and select your subscription account
+# # view and select your subscription account
 
-az account list -o table
+# az account list -o table
 
-export SUBSCRIPTION="$(az account show -o json | jq -r '.id')"
+# export SUBSCRIPTION="$(az account show -o json | jq -r '.id')"
 
-echo "SETTING ACCOUNT"
-az account set --subscription ${serviceprincipalsubscription}
+# echo "SETTING ACCOUNT"
+# az account set --subscription ${serviceprincipalsubscription}
 
 
-#creating service principal
+# #creating service principal
 
-SERVICE_PRINCIPAL_JSON="$(az ad sp create-for-rbac --skip-assignment --name petstore_service_principal -o json)"
+# SERVICE_PRINCIPAL_JSON="$(az ad sp create-for-rbac --skip-assignment --name petstore_service_principal -o json)"
 
-export SERVICE_PRINCIPAL="$(echo $SERVICE_PRINCIPAL_JSON | jq -r '.appId')"
-export SERVICE_PRINCIPAL_SECRET="$(echo $SERVICE_PRINCIPAL_JSON | jq -r '.password')"
+# export SERVICE_PRINCIPAL="$(echo $SERVICE_PRINCIPAL_JSON | jq -r '.appId')"
+# export SERVICE_PRINCIPAL_SECRET="$(echo $SERVICE_PRINCIPAL_JSON | jq -r '.password')"
 
-# Grant contributor role over the subscription to our service principal
+# # Grant contributor role over the subscription to our service principal
 
-echo "SETTING ROLE ASSIGNMENT"
+# echo "SETTING ROLE ASSIGNMENT"
 
-az role assignment create --assignee ${serviceprincipalclientid} \
---scope "/subscriptions/${serviceprincipalsubscription}" \
---role Contributor
+# az role assignment create --assignee ${serviceprincipalclientid} \
+# --scope "/subscriptions/${serviceprincipalsubscription}" \
+# --role Contributor
 
 
 echo "CDing into terraform folder"
-cd ~/Final-Project-/terraform
+
+cd terraform
 
 #terraform
+
+export ARM_USE_MSI=true
+export ARM_SUBSCRIPTION_ID=$(az account show -o json | jq -r '.id')
+export ARM_TENANT_ID=$(az account show | jq -r '.tenantId')
+
 terraform init
 
-terraform plan -out plan.out -var serviceprinciple_id=${serviceprincipalclientid} \
-    -var serviceprinciple_key="${serviceprincipalpassword}" \
-    -var tenant_id=${serviceprincipaltenant} \
-    -var subscription_id=${serviceprincipalsubscription} \
+ terraform plan -out plan.out 
+#-var serviceprinciple_id=${serviceprincipalclientid} \
+#     -var serviceprinciple_key="${serviceprincipalpassword}" \
+#     -var tenant_id=${serviceprincipaltenant} \
+#     -var subscription_id=${serviceprincipalsubscription} \
 
 terraform apply plan.out
 
-$ terraform output kube_config | grep -i -v -E '<<EOT|EOT' > ~/.kube/aksconfig
-$ export KUBECONFIG=~/.kube/aksconfig
+terraform output kube_config | grep -i -v -E '<<EOT|EOT' > ~/.kube/aksconfig
+export KUBECONFIG=~/.kube/aksconfig
 
 
-Test configuration using kubectl
+# Test configuration using kubectl
 
-$ kubectl get nodes
+kubectl get nodes
 
 #cd into the yaml folders
 cd ${workspace}
@@ -60,7 +67,7 @@ echo $(pwd)
 echo $(ls -a)
 
 
-az aks get-credentials --resource-group" rg" --name "cluster name" --overwrite-existing
+#az aks get-credentials --resource-group" rg" --name "cluster name" --overwrite-existing
 
 kubectl apply -f k8s/nginx.yaml
 kubectl apply -f k8s/frontend.yaml
